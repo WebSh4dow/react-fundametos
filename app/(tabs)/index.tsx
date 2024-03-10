d645284c317cd2d903ee3c56b59af1b92ec8ecf7
jarmison.paiva <jarmison6@gmail.com>
2024-03-10 00:13:38 -0300

    Estilizando Elementos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,29 @@
-import { StyleSheet } from 'react-native';
+import {Text, View} from 'react-native';
+import {Fragment} from 'react';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function TabOneScreen(){
+    return(
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+   <View style={{
+        flex: 1,
+        backgroundColor: '#131016'
+    }}>
+
+    <Text style= {{
+      color: '#FDFCFE',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 48
+      }}>
+      Nome do Evento
+    </Text>
+
+    <Text style= {{
+      color: '#6B6B6B',
+      fontSize: 16
+    }}>
+      Sexta, 9 de Março de 2024
+   </Text>
+ </View>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
